9aa7263238efb7dd5d8779af6f5a165fd3438e3c
Rozas Guy-David <rozasguydavid@gmail.com>
2021-12-10 04:52:44 +0100

    premier commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <h1>Eventdrive</h1>
+    <Events/>
+  </div>
+</template>
+
+<script>
+import Events from './components/Events'
+
+export default {
+  name: 'App',
+  components: {
+    Events
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Events.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="events">
+        <h2>{{ connect }}</h2>
+        test
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Events',
+  data () {
+    return {
+      connect: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
