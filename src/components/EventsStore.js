d55a20c04e7c754cfd3407d9985396130ef0f67d
Rozas Guy-David <rozasguydavid@gmail.com>
2021-12-14 12:15:18 +0100

    fix de EventStore.js

--- src/components/EventsStore.js
+++ src/components/EventsStore.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
-import jwtDecode from 'jwt-decode'
 import Vuex from 'vuex'
 
 const state = {
   // J'initialise mon jeton en verifiant l'existant dans le cache
-  accessToken: localStorage.getItem('access_token') ? localStorage.getItem('access_token') : '' || null,
+  accessToken: '' || null,
   refreshToken: '' || null,
-  eventList: [] || null,
+  eventList: [],
   showCreateEvent: false,
   showUpdateEvent: false
 }
@@ -14,34 +13,40 @@ const state = {
 const mutations = {
   // Je définit les evenement à déclancher pour :
   // Recuper mon jeton depuis le cache
-  loginUser: (state) => {
-    state.accessToken = localStorage.getItem('access_token')
-    state.refreshToken = localStorage.getItem('refresh_token')
+  TokenMut: (state, token) => {
+    state.accessToken = token.access_token
+    state.refreshToken = token.refresh_token
   },
+
   // Détruire mon jeton
   destroyToken: (state) => {
     state.accessToken = null
     state.refreshToken = null
   },
+
   // Recupérer ma liste d'evenement
-  getEvents: (state) => {
-    state.eventList = JSON.parse(localStorage.getItem('event_list'))
+  eventsMut: (state, eventList) => {
+    state.eventList = JSON.parse(eventList)
   },
+
   // Montrer le formulaire d'ajout d'évenement
   showCreateEvent: (state) => {
     state.showUpdateEvent = false
     state.showCreateEvent = true
   },
+
   // Cacher le formulaire d'ajout d'évenement
   hideCreateEvent: (state) => {
     state.showUpdateEvent = false
     state.showCreateEvent = false
   },
+
   // Montrer le formulaire d'update d'évenement
   showUpdateEvent: (state) => {
     state.showCreateEvent = false
     state.showUpdateEvent = true
   },
+
   // Cacher le formulaire d'update d'évenement
   hideUpdateEvent: (state) => {
     state.showCreateEvent = false
@@ -61,12 +66,11 @@ const actions = {
         client_secret: fields.clientSecret
       })
         .then(response => {
-          // Je stock mon jeton dans le cache
-          localStorage.setItem('access_token', response.data.access_token)
-          localStorage.setItem('refresh_token', response.data.refresh_token)
-
-          // J'appelle ma mutation loginUser qui mettra à jour mon jeton depuis mon cache
-          store.commit('loginUser')
+          // J'appelle ma mutation TokenMut qui mettra à jour mon jeton dans le store
+          store.commit('TokenMut', {
+            access_token: response.data.access_token,
+            refresh_token: response.data.refresh_token
+          })
           resolve(response)
         })
         .catch(error => {
@@ -94,8 +98,7 @@ const actions = {
         }
       })
         .then(response => {
-          localStorage.setItem('event_list', JSON.stringify(response.data))
-          store.commit('getEvents')
+          store.commit('eventsMut', JSON.stringify(response.data))
         })
         .catch(error => {
           // en cas d'erreur je renvoie le message
@@ -106,9 +109,6 @@ const actions = {
   },
 
   createEvent: (store, requestFields) => {
-    // TODO faire une requette create
-    console.log("créer l'évenement " + requestFields)
-
     let data = {
       main_manager_id: requestFields.mainManagerId,
       name: {
@@ -139,6 +139,7 @@ const actions = {
         })
     })
   },
+
   updateEvent: (store, requestFields) => {
     let data = {
       main_manager_id: requestFields.mainManagerId,
@@ -169,6 +170,7 @@ const actions = {
         })
     })
   },
+
   removeEvent: (store, id) => {
     if (confirm("voullez vous vraiment supprimer l'évenement ")) {
       return new Promise((resolve, reject) => {
@@ -196,40 +198,21 @@ const actions = {
     return getters.loggedIn ? '' : dispatch('loginUser', {id: fields.id, clientSecret: fields.clientSecret})
   },
 
-  eventOrCached: (store) => {
+  eventOrCached: ({ dispatch, commit, getters }) => {
     // Je verifie qu'une liste existe dans le cache
-    if (!localStorage.event_list) {
+    if (getters.hasEvents === false) {
       // requette ur l'Api
-      store.dispatch('getEvents')
-    } else {
-      // Evenement dans le store
-      store.commit('getEvents')
+      dispatch('getEvents')
     }
-  },
-
-  refreshToken: (store) => {
-    store.dispatch('loginUser')
-    store.dispatch('autoRefresh')
-  },
-
-  autoRefresh: (store) => {
-    // je recupère le temps restant en decodant le jeton
-    const { exp } = jwtDecode(store.state.accessToken)
-
-    const now = Date.now() / 1000
-    let timeUntilRefresh = exp - now
-
-    // Je reduit mon le temps restant de 15 min avant de raffraichir,
-    timeUntilRefresh -= (15 * 60)
-    const refreshToken = setTimeout(() => store.dispatch('refreshToken'), Math.round(timeUntilRefresh))
   }
 }
 
 const getters = {
   // Je défini les getters que je vais pouvoir déclancher notament depuis ma vue Events
-  loggedIn: state => state.accessToken != null,
-  // TODO FIX hasEvent
-  hasEvents: state => state.eventList.length !== 0,
+  hasToken: state => state.accessToken !== null,
+  loggedIn: (state, getters) => getters.hasToken !== false,
+  eventCount: state => state.eventList.length,
+  hasEvents: (state, getters) => getters.eventCount !== 0,
   createShowed: state => state.showCreateEvent !== false,
   updateShowed: state => state.showUpdateEvent !== false,
   getEventStoredById: (state) => (id) => {
